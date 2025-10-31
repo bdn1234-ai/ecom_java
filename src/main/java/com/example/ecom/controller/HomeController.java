d8cf980ce959b1b5a0973dabbabbdc9f33e4d69f
huyicetea <huy7trung@gmail.com>
2025-10-31 10:05:16 +0700

    Merge branch 'master' into huy/251030_view_product_category_wise the commit.

--- src/main/java/com/example/ecom/controller/HomeController.java
+++ src/main/java/com/example/ecom/controller/HomeController.java
@@ -2,12 +2,9 @@ package com.example.ecom.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.ecom.model.Product;
 import com.example.ecom.service.CategoryService;
 import com.example.ecom.service.ProductService;
 
@@ -23,12 +20,5 @@ public class HomeController {
     @GetMapping
     public String home() {
         return "home";
-    }
-
-    @GetMapping("/product/{id}")
-    public String product(@PathVariable int id, Model m) {
-        Product productById = productService.getProductById(id);
-        m.addAttribute("product", productById);
-        return "view_product";
     }
 }
--- src/main/java/com/example/ecom/controller/ProductController.java
+++ src/main/java/com/example/ecom/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,5 +33,12 @@ public class ProductController {
         m.addAttribute("products", products);
         m.addAttribute("paramValue", category);
         return "product"; //trả về tên của sản phẩm
+    }
+
+    @GetMapping("/product/{id}")
+    public String product(@PathVariable int id, Model m) {
+        Product productById = productService.getProductById(id);
+        m.addAttribute("product", productById);
+        return "view_product";
     }
 }
