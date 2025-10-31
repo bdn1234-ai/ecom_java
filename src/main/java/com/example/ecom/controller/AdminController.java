392db2876f19c62fd9556510a5fb061ec1cfb84b
Bachchuy <khanh.hq202416239@sis.hust.edu.vn>
2025-10-31 22:10:03 +0700

    Add missing import for Model in AdminController

--- src/main/java/com/example/ecom/controller/AdminController.java
+++ src/main/java/com/example/ecom/controller/AdminController.java
@@ -26,6 +26,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import org.springframework.ui.Model;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
